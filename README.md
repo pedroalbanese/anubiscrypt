bc5fb76c23a04397d38219e4f2c066614620f7a1
Pedro F. Albanese <68971450+pedroalbanese@users.noreply.github.com>
2022-08-21 18:27:18 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# anubiscrypt
-Anubis Block Cipher Encryption Tool
+# AESCrypt
+[![ISC License](http://img.shields.io/badge/license-ISC-blue.svg)](https://github.com/pedroalbanese/anubiscrypt/blob/master/LICENSE.md) 
+[![GoDoc](https://godoc.org/github.com/pedroalbanese/anubiscrypt?status.png)](http://godoc.org/github.com/pedroalbanese/anubiscrypt)
+[![Go Report Card](https://goreportcard.com/badge/github.com/pedroalbanese/anubiscrypt)](https://goreportcard.com/report/github.com/pedroalbanese/anubiscrypt)
+[![GitHub go.mod Go version](https://img.shields.io/github/go-mod/go-version/pedroalbanese/anubiscrypt)](https://golang.org)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/pedroalbanese/anubiscrypt)](https://github.com/pedroalbanese/anubiscrypt/releases)  
+
+Barreto & Rijmen 128-bit block cipher in EAX Mode provides both authenticated encryption (confidentiality and authentication) and the ability to check the integrity and authentication of additional authenticated data (AAD) that is sent in the clear. AES-GCM is specified in NIST Special Publication 800-38D ([SP800-38D](https://csrc.nist.gov/publications/detail/sp/800-38d/final)).
+### Command-line Anubis-EAX Encryption Tool
+<pre>anubiscrypt [-d] [-b N] -p "pass" [-i N] [-s "salt"] -f <file.ext>
+  -b int
+        Key length: 128, 192 or 256. (default 256)
+  -c string
+        Cipher: AES, RC6, Twofish or Serpent. (default "aes")
+  -d    Decrypt instead Encrypt.
+  -f string
+        Target file. ('-' for STDIN)
+  -i int
+        Iterations. (for PBKDF2) (default 1024)
+  -k string
+        Symmetric key to Encrypt/Decrypt.
+  -m    Cipher-based message authentication code.
+  -p string
+        Password-based key derivation function 2.
+  -r    Generate random cryptographic key with given bit-length.
+  -s string
+        Salt. (for PBKDF2)</pre>
+
+## License
+
+This project is licensed under the ISC License.
+
+##### Industril-Grade Reliability. Copyright (c) 2020-2022 ALBANESE Research Lab.
---
+++ cmd/anubiscrypt/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"bytes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"golang.org/x/crypto/pbkdf2"
+	"io"
+	"log"
+	"os"
+
+	"github.com/dgryski/go-anubis"
+	"github.com/pedroalbanese/cmac"
+	"github.com/pedroalbanese/edgetk/eax"
+)
+
+var (
+	dec    = flag.Bool("d", false, "Decrypt instead Encrypt.")
+	file   = flag.String("f", "", "Target file. ('-' for STDIN)")
+	iter   = flag.Int("i", 1024, "Iterations. (for PBKDF2)")
+	key    = flag.String("k", "", "Symmetric key to Encrypt/Decrypt.")
+	length = flag.Int("b", 256, "Key length: 128, 192 or 256.")
+	mac    = flag.Bool("m", false, "Cipher-based message authentication code.")
+	pbkdf  = flag.String("p", "", "Password-based key derivation function 2.")
+	random = flag.Bool("r", false, "Generate random cryptographic key with given bit-length.")
+	salt   = flag.String("s", "", "Salt. (for PBKDF2)")
+)
+
+func main() {
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "AnubisCrypt - ALBANESE Research Lab (c) 2020-2022 ")
+		fmt.Fprintln(os.Stderr, "Barreto & Rijmen 128-bit block cipher in EAX Mode\n")
+		fmt.Fprintln(os.Stderr, "Usage of "+os.Args[0]+":")
+		fmt.Fprintln(os.Stderr, os.Args[0]+" [-d] [-b N] -p \"pass\" [-i N] [-s \"salt\"] -f <file.ext>")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *random == true {
+		var key []byte
+		var err error
+		key = make([]byte, *length/8)
+		_, err = io.ReadFull(rand.Reader, key)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(hex.EncodeToString(key))
+		os.Exit(0)
+	}
+
+	var keyHex string
+	var prvRaw []byte
+
+	if *mac {
+		if *pbkdf != "" {
+			prvRaw = pbkdf2.Key([]byte(*pbkdf), []byte(*salt), *iter, 16, sha256.New)
+			keyHex = hex.EncodeToString(prvRaw)
+		} else {
+			keyHex = *key
+		}
+		var err error
+		var ciph cipher.Block
+		ciph = anubis.New([]byte(keyHex))
+		if err != nil {
+			log.Fatal(err)
+		}
+		h, _ := cmac.New(ciph)
+		var data io.Reader
+		if *file == "-" {
+			data = os.Stdin
+		} else {
+			data, _ = os.Open(*file)
+		}
+		io.Copy(h, data)
+		fmt.Println(hex.EncodeToString(h.Sum(nil)))
+		os.Exit(0)
+	} else {
+		if *pbkdf != "" {
+			prvRaw = pbkdf2.Key([]byte(*pbkdf), []byte(*salt), *iter, *length/8, sha256.New)
+			keyHex = hex.EncodeToString(prvRaw)
+		} else {
+			keyHex = *key
+		}
+		var key []byte
+		var err error
+		if keyHex == "" {
+			key = make([]byte, *length/8)
+			_, err = io.ReadFull(rand.Reader, key)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Fprintln(os.Stderr, "Key=", hex.EncodeToString(key))
+		} else {
+			key, err = hex.DecodeString(keyHex)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if len(key) != 32 && len(key) != 24 && len(key) != 16 {
+				log.Fatal(err)
+			}
+		}
+
+		buf := bytes.NewBuffer(nil)
+		var data io.Reader
+		if *file == "-" {
+			data = os.Stdin
+		} else {
+			data, _ = os.Open(*file)
+		}
+		io.Copy(buf, data)
+		msg := buf.Bytes()
+
+		var c cipher.Block
+		c = anubis.New(key)
+		aead, _ := eax.NewEAX(c)
+
+		if *dec == false {
+			nonce := make([]byte, aead.NonceSize(), aead.NonceSize()+len(msg)+aead.Overhead())
+
+			out := aead.Seal(nonce, nonce, msg, nil)
+			fmt.Printf("%s", out)
+
+			os.Exit(0)
+		}
+
+		if *dec == true {
+			nonce, msg := msg[:aead.NonceSize()], msg[aead.NonceSize():]
+
+			out, err := aead.Open(nil, nonce, msg, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("%s", out)
+
+			os.Exit(0)
+		}
+	}
+}
